7444ebb7690fca97268b67c747aa20ce3dd874c3
Michael Neale <michael.neale@gmail.com>
2019-10-23 17:13:00 +1100

    gcloud deploying

--- main.py
+++ main.py
@@ -8,24 +8,34 @@ from rox.server.rox_server import Rox
 import time
 
 
+def current_time():
+    return int(round(time.time() * 1000))
 
+def stopwatch(start_time, msg):
+    print(msg + ": " + str(current_time() - start_time))    
+
+
+
+# 
+# Feature flags state is managed and updated live by Rollout
+# Treat it as read only in your function. 
+# Even though it is setup once - even if your function is invoked "warm", rollout will 
+# still ensure the state of the flags represents the current Rollout console config in the background. 
+#
 class MyContainer:
      def __init__(self):
         self.spankyChat = RoxFlag()
-
 my_container = MyContainer()
-print("Registering "+  str(int(round(time.time() * 1000))))
 
+
+#
+# Rollout flag setup (called once for a cold start)
+#
 Rox.register('', my_container)
-print("Registered "+  str(int(round(time.time() * 1000))))
-print("Calling setup "+  str(int(round(time.time() * 1000))))
-
-start = int(round(time.time() * 1000))
-cancel_event = Rox.setup("5c528ffeaf23cf07f9bb3a85").result()
-#Rox.setup("5c528ffeaf23cf07f9bb3a85")
-print("Finished setup "+  str(int(round(time.time() * 1000))))
-
-print("Time taken " + str(int(round(time.time() * 1000)) - start))
+start = current_time()
+#cancel_event = Rox.setup("5c528ffeaf23cf07f9bb3a85").result()
+Rox.setup("5c528ffeaf23cf07f9bb3a85")
+stopwatch(start, "Setup time")
 
 
 if (my_container.spankyChat.is_enabled() == True):
@@ -34,25 +44,18 @@ else:
     print('NOT ENABLED')
 
 
-
-def rollout_webhook(request):
+#
+# Everything until this point is called once during a "cold start" of the function. 
+# 
+#
+def my_function(request):
     """HTTP Cloud Function. Using: http://flask.pocoo.org/docs/1.0/api/#flask.Request
-
-    This will take a webhook and kill the apporopriate rollout experiment (thereby halting the rollout!)
-
-    USAGE and parameters: 
-        Query parmeters are used to pass the rollout app and secret details. 
-        POST or GET to a URL once deployed as a google function or similar:
-            https://DEPLOYED_FUNCTION_URL?secret=API_Token&app_id=YOUR_APP_ID&environment_name=YOUR_ENV_NAME&flag_name=YOUR_FLAGN_AME
-
-    For example: 
-        curl -X POST "https://us-central1-micprojects.cloudfunctions.net/rollout_webhook?secret=..&app_id=..&environment_name=..&flag_name=.."
 
     Rollout setup info: 
         See https://support.rollout.io/reference for details on how to get your app id and api token.
 
     Google cloud function deploying: 
-        gcloud functions deploy rollout_webhook --trigger-http --runtime "python37"    
+        gcloud functions deploy my_function --trigger-http --runtime "python37"    
 
     """
 
